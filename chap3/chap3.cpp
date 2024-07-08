b14abab6d0e290e0ba4553ca57259992170dda30
dpwls <dpwls02142@gmail.com>
2024-07-08 19:45:09 +0900

    chore : 3강_76번 코드 까지

--- chap3/chap3.cpp
+++ chap3/chap3.cpp
@@ -1,4 +1,6 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 
 // void assign10(void);
 // void assign20(void);
@@ -6,6 +8,46 @@
 
 // void auto_func(void);
 // void static_func(void);
+
+struct student
+{
+	int num;
+	double grade;
+};
+
+struct profile {
+	char name[20];
+	int age;
+	double height;
+	char* info;
+};
+/*
+struct student
+{
+	int id;
+	char name[20];
+	double grade;
+};
+*/
+struct vision {
+	double left;
+	double right;
+};
+
+struct vision exchange(struct vision robot);
+
+struct address {
+	char name[20];
+	int age;
+	char tel[20];
+	char addr[80];
+};
+
+enum season { spring, sumer, fall, winter };
+
+typedef struct student Student;
+
+void print_data(Student* ps);
 
 int main(void) {
 
@@ -77,8 +119,114 @@ int main(void) {
 	}
 	*/
 
-	// 코드 67
+	// 코드 69
+	/*
+	struct student s1;
+	s1.num = 2;
+	s1.grade = 2.7;
+	printf("학번: %d\n", s1.num);
+	printf("학점: %.1lf\n", s1.grade);
+	*/
 
+	// 코드 70
+	/*
+	struct profile yuni;
+
+	strcpy_s(yuni.name, 20, "서하윤");
+	yuni.age = 17;
+	yuni.height = 164.5;
+
+	yuni.info = (char*)malloc(80);
+	printf("자기소개: ");
+
+	if (yuni.info != NULL) {
+		gets_s(yuni.info, sizeof(yuni.info));
+	}
+	printf("이름: %s\n", yuni.name);
+	printf("나이: %d\n", yuni.age);
+	printf("키: %.1lf\n", yuni.height);
+	if (yuni.info != NULL) {
+		printf("자기소개: %s\n", yuni.info);
+	}
+	free(yuni.info);
+	*/
+
+	// 코드 72
+	/*
+	struct student s1 = { 315, "홍길동", 2.4 };
+	struct student s2 = { 316, "이순신", 3.4 };
+	struct student s3 = { 317, "세종대왕", 4.4 };
+
+	struct student max;
+
+	max = s1;
+
+	if (s2.grade > max.grade) {
+		max = s2;
+	}
+	if (s3.grade > max.grade) {
+		max = s3;
+	}
+
+	printf("학번: %d\n", max.id);
+	printf("이름: %s\n", max.name);
+	printf("학점: %.1lf\n", max.grade);
+	*/
+
+	// 코드 73
+	/*
+	struct vision robot;
+	printf("양쪽 시력: ");
+	scanf_s("%lf %lf", &(robot.left), &(robot.right));
+
+	robot = exchange(robot);
+	printf("바뀐 시력: %.1lf %.1lf", robot.left, robot.right);
+	*/
+	
+	// 코드 74
+	/*
+	struct address list[5] = {
+	{"홍길동", 23, "111-1111", "서울 송파"},
+	{"풍월량", 22, "211-1111", "인천 송도"},
+	{"호시노겐", 23, "311-1111", "일본 도쿄"},
+	{"ㅎㅇ", 24, "411-1111", "서울 건대"},
+	{"ㅇㅇ", 25, "511-1111", "서울 홍대"},
+	};
+
+	int i;
+
+	for (i = 0; i < 5; i++) {
+		printf("%10s %5d %15s %20s\n", list[i].name, list[i].age, list[i].tel, list[i].addr);
+	}
+	*/
+
+	// 코드 75
+	/*
+	enum season ss;
+	char* pc = NULL;
+
+	ss = spring;
+	switch (ss) {
+	case spring:
+		pc = (char*)"inline";
+		break;
+	case sumer:
+		pc = (char*)"swimming";
+		break;
+	case fall:
+		pc = (char*)"trip";
+		break;
+	case winter:
+		pc = (char*)"skiing";
+		break;
+	}
+	printf("레저 활동 => %s\n", pc);
+	*/
+	
+	// 코드 76
+	Student s1 = { 315, 4.2 };
+	print_data(&s1);
+	
 	return 0;
 
 }
@@ -106,3 +254,18 @@ void static_func(void) {
 	printf("%d", a);
 }
 */
+
+struct vision exchange(struct vision robot) {
+	double temp;
+	temp = robot.left;
+	robot.left = robot.right;
+	robot.right = temp;
+
+	return robot;
+}
+
+void print_data(Student* ps)
+{
+	printf("학번: %d\n", ps->num);
+	printf("학점: %.1lf\n", ps->grade);
+}
