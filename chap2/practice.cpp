0f5439c0b331b2ea51ac02413a4985b37c65248b
dpwls <dpwls02142@gmail.com>
2024-07-08 21:15:26 +0900

    feat : 연습문제_62번 코드까지 풂

--- chap2/practice.cpp
+++ chap2/practice.cpp
@@ -1,11 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include<string.h>
 
 void calculator(int* a, int* b, int* sum, int* sub, int* mult, int* div, int* rem);
 void admission(int* audience, int* seat, double* rate);
 void sum(int* a, int *res);
 void quadrant(int* a, int* b);
 void leapYear(int* a);
+void diceGame();
+int bep(int* fcost, int* vcost, int* sell);
+int sugar(int* weight);
+void factorial(int* n, int* res);
+void add_three_numbers(int a, int b, int c) {
+	static int sum = 0;
+	sum = a + b + c;
+	printf("합: %d\n", sum);
+}
+
+void swap(int* num1, int* num2, int* num3) {
+	int temp = *num1;
+	*num1 = *num2;
+	*num2 = *num3;
+	*num3 = temp;
+}
+
+int isPalindrome(char word[]) {
+	int len = strlen(word);
+
+	for (int i = 0; i < len / 2; i++) {
+		if (word[i] != word[len - 1 - i]) {
+			return 0;
+		}
+	}
+
+	return 1;
+}
+
+struct employee {
+	char name[50];
+	int id;
+	int salary;
+};
 
 int main(void) {
 
@@ -293,10 +328,12 @@ int main(void) {
 	*/
 	
 	// 연습 20
+	/*
 	int a;
 	scanf_s("%d", &a);
 
 	leapYear(&a);
+	*/
 
 	// 연습 21
 	/*
@@ -411,7 +448,39 @@ int main(void) {
 	*/
 
 	// 연습 28
+	// diceGame();
 
+	// 연습 29
+	/*
+	int fcost, vcost, sell;
+	scanf_s("%d %d %d", &fcost, &vcost, &sell);
+
+	int cnt = bep(&fcost, &vcost, &sell);
+
+	printf("%d", cnt);
+	*/
+	
+	// 연습 30
+	/*
+	int weight;
+	scanf_s("%d", &weight);
+
+	int res = sugar(&weight);
+	printf("%d", res);
+	*/
+
+	// 연습 31
+	/*
+	int n, res;
+	scanf_s("%d", &n);
+
+	if (n < 0 || n>12) {
+		return 1;
+	}
+
+	factorial(&n, &res);
+	printf("%d", res);
+	*/
 
 	// 연습 32.
 	/*
@@ -423,6 +492,31 @@ int main(void) {
 	else if (a % 2 != 0) {
 		printf("홀수");
 	}
+	*/
+
+	// 연습 33.
+	/*
+	int a, b;
+	scanf_s("%d %d", &a, &b);
+	if (abs(a) > abs(b)) {
+		printf("%d", a);
+	}
+	else {
+		printf("%d", b);
+	}
+	*/
+
+	// 연습 34
+	/*
+	int sum = 0;
+	int i = 1;
+	while (i <= 100) {
+		if (i % 3 == 0) {
+			sum += i;
+		}
+		i++;
+	}
+	printf("%d", sum);
 	*/
 
 	// 연습 35.
@@ -438,23 +532,458 @@ int main(void) {
 	printf("%d", sum);
 	*/
 
+	// 연습 36
+	/*
+	int sum = 0;
+	int i = 1;
+	do {
+		if (i % 3 == 0) {
+			sum += i;
+		}
+		i++;
+	} while (i <= 100);
+	printf("%d", sum);
+	*/
+
 	// 연습 37.
 	/*
-	int a, i;
-	scanf_s("%d", &a);
-	for (i = 0; i <= a; i++) {
-		if (i % 2 == 0) {
-			printf("%d", a);
-		}
-		if (i % 5 == 0) {
+	int n;
+	scanf_s("%d", &n);
+	int sum = 0;
+	for (int i = 1; i <= n; i++) {
+		if (i % 2 != 0 || i % 5 == 0) {
 			continue;
+		}
+		sum += i;
+	}
+	printf("%d", sum);
+	*/
+
+	// 연습 38
+	/*
+	int num;
+	int max = 1;
+
+	while (true) {
+
+		scanf_s("%d", &num);
+
+		if (num == -1) {
+			break;
+		}
+
+		if (num > max) {
+			max = num;
+		}
+	}
+	printf("%d", max);
+	*/
+
+	// 연습 39
+	/*
+	int days_in_months[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	int month, day;
+	scanf_s("%d %d", &month, &day);
+	int total_days = 0;
+
+	for (int i = 0; i < month - 1; i++) {
+		total_days += days_in_months[i];
+	}
+	total_days += day;
+
+	printf("%d", total_days);
+	*/
+
+	// 연습 40
+	/*
+	int scores[10];
+	for (int i = 0; i < 10; i++) {
+		scanf_s("%d", &scores[i]);
+	}
+
+	int sum = 0;
+	int max = scores[0];
+	int min = scores[0];
+	int size = 10;
+
+	for (int i = 0; i < size; i++) {
+		sum += scores[i];
+		if (scores[i] > max) {
+			max = scores[i];
+		}
+		if (scores[i] < min) {
+			min = scores[i];
+		}
+	}
+
+	printf("총합: %d\n", sum);
+	printf("평균: %.2f\n", (float)sum / size);
+	printf("최고 점수: %d\n", max);
+	printf("최저 점수: %d\n", min);
+	*/
+
+	// 연습 41
+	/*
+	int x1, y1, x2, y2;
+
+	scanf_s("%d %d", &x1, &y1);
+
+	scanf_s("%d %d", &x2, &y2);
+
+	int width = abs(x2 - x1);
+	int height = abs(y2 - y1);
+	int area = width * height;
+
+	printf("사각형의 넓이: %d\n", area);
+	*/
+
+	// 연습 42
+	/*
+	int sum = 0, count = 0, num;
+
+	while (true) {
+		
+		scanf_s("%d", &num);
+
+		if (num == 0) {
+			break;
+		}
+		sum += num;
+		count++;
+	}
+
+	printf("합: %d, 평균: %.2f\n", sum, (double)sum / count);
+	*/
+	
+	// 연습 43
+	/*
+	int n, sum = 0;
+	scanf_s("%d", &n);
+	for (int i = 1; i <= n; i++) {
+		if (i % 2 == 0 && i % 3 != 0) {
+			sum += i;
+		}
+	}
+	printf("%d", sum);
+	*/
+
+	// 연습 44
+	/*
+	int a, b, sum = 0;
+	scanf_s("%d %d", &a, &b);
+	if (a > b) {
+		int temp = a;
+		a = b;
+		b = temp;
+	}
+	for (int i = a; i <= b;i++) {
+		sum += i;
+	}
+	printf("%d", sum);
+	*/
+
+	// 연습 45
+	/*
+	int n, gob = 1;
+	scanf_s("%d", &n);
+	for (int i = 1; i <= n;i++) {
+		gob *= i;
+	}
+	printf("%d", gob);
+	*/
+	
+	// 연습 46
+	/*
+	for (int i = 1; i <= 100; i++) {
+		if (i % 7 == 0 || i % 9 == 0) {
+			printf("%d\n", i);
 		}
 	}
 	*/
 
+	// 연습 47
+	/*
+	int a, b;
+	scanf_s("%d %d", &a, &b);
+	if (a > b) {
+		printf("%d", a - b);
+	}
+	else {
+		printf("%d", b - a);
+	}
+	*/
+
+	// 연습 48
+	/*
+	for (int A = 0; A <= 9; A++) {
+		for (int Z = 0; Z <= 9; Z++) {
+			if (10 * A + Z + 10 * Z + A == 99) {
+				printf("A = %d, Z = %d\n", A, Z);
+			}
+		}
+	}
+	*/
+
+	// 연습 49
+	/*
+	int n;
+
+	scanf_s("%d", &n);
+
+	int a = 0, b = 1, next;
+
+	for (int i = 0; i < n; i++) {
+		if (i == 0) {
+			printf("%d", a);
+		}
+		else if (i == 1) {
+			printf(" %d", b);
+		}
+		else {
+			next = a + b;
+			a = b;
+			b = next;
+			printf(" %d", next);
+		}
+	}
+	printf("\n");
+	*/
+
+	// 연습 50
+	/*
+	int x, y, z;
+
+	scanf_s("%d %d %d", &x, &y, &z);
+
+	add_three_numbers(x, y, z);
+	*/
+
+	// 연습 51
+	/*
+	int total_seconds;
+	int hours, minutes, seconds;
+
+	scanf_s("%d", &total_seconds);
+
+	hours = total_seconds / 3600;
+	minutes = (total_seconds % 3600) / 60;
+	seconds = total_seconds % 60;
+
+	printf("h:%d m:%d s:%d\n", hours, minutes, seconds);
+	*/
+
+	// 연습 52
+	/*
+	int n;
+	int result = 1;
+
+	scanf("%d", &n);
+
+	for (int i = 0; i < n; i++) {
+		result *= 2;
+	}
+
+	printf("%d", result);
+	*/
+	
+	// 연습 53
+	/*
+	int arr[5];
+	int max, min, sum = 0;
+
+	for (int i = 0; i < 5; i++) {
+		scanf_s("%d", &arr[i]);
+	}
+
+	max = arr[0];
+	min = arr[0];
+
+	for (int i = 0; i < 5; i++) {
+		if (arr[i] > max) {
+			max = arr[i];
+		}
+		if (arr[i] < min) {
+			min = arr[i];
+		}
+		sum += arr[i];
+	}
+
+	printf("최댓값: %d\n", max);
+	printf("최솟값: %d\n", min);
+	printf("합: %d\n", sum);
+	*/
+
+	// 코드 54
+	/*
+	char word[100];
+	char max_char;
+
+	scanf_s("%s", word);
+
+	max_char = word[0];
+	for (int i = 1; i < strlen(word); i++) {
+		if (word[i] > max_char) {
+			max_char = word[i];
+		}
+	}
+
+	printf("%c", max_char);
+	*/
+
+	// 코드 55
+	/*
+	int num1, num2, num3;
+	scanf_s("%d %d %d", &num1, &num2, &num3);
+
+	swap(&num1, &num2, &num3);
+
+	printf("바뀐 값: num1 = %d, num2 = %d, num3 = %d\n", num1, num2, num3);
+	*/
+
+	// 코드 56
+	/*
+	int arr[10], odd[10], even[10];
+	int odd_count = 0, even_count = 0;
+
+	for (int i = 0; i < 10; i++) {
+		scanf_s("%d", &arr[i]);
+	}
+
+	for (int i = 0; i < 10; i++) {
+		if (arr[i] % 2 != 0) {
+			odd[odd_count++] = arr[i];
+		}
+		else {
+			even[even_count++] = arr[i];
+		}
+	}
+
+	for (int i = 0; i < odd_count; i++) {
+		printf("%d ", odd[i]);
+	}
+	for (int i = even_count - 1; i >= 0; i--) {
+		printf("%d ", even[i]);
+	}
+	printf("\n");
+	*/
+	
+	// 코드 57
+	/*
+	char word[100];
+
+	scanf_s("%s", word);
+
+	if (isPalindrome(word)) {
+		printf("1");
+	}
+	else {
+		printf("0");
+	}
+	*/
+	
+	// 코드 58
+	/*
+	int record[4][4]; 
+	char names[4][10] = { "철희", "철수", "영희", "영수" };
+	int total[4] = { 0 }; 
+
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			scanf_s("%d", &record[i][j]);
+			total[i] += record[i][j]; 
+		}
+	}
+
+	for (int i = 0; i < 4; i++) {
+		printf("%s의 총점: %d\n", names[i], total[i]);
+	}
+	*/
+	
+	// 코드 59
+	/*
+	int arr[4][4];
+	int reversed[4][4];
+	int count = 1;
+
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			scanf_s("%d", &arr[i][j]);
+		}
+	}
+
+	for (int i = 3; i >= 0; i--) {
+		for (int j = 3; j >= 0; j--) {
+			reversed[3 - i][3 - j] = arr[i][j];
+		}
+	}
+
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			printf("%d ", reversed[i][j]);
+		}
+	}
+	*/
+
+	// 코드 60
+	/*
+	char ch;
+
+	scanf_s(" %c", &ch);
+
+	if ('A' <= ch && ch <= 'Z') {
+		printf("%c의 소문자: %c\n", ch, ch + ('a' - 'A'));
+	}
+	else if ('a' <= ch && ch <= 'z') {
+		printf("%c의 대문자: %c\n", ch, ch - ('a' - 'A'));
+	}
+	*/
+
+	// 코드 61
+	/*
+	struct employee emp;
+
+	printf("이름: ");
+	scanf_s("%s", emp.name, 50);
+
+	printf("고유번호: ");
+	scanf_s("%d", &emp.id);
+
+	printf("급여정보: ");
+	scanf_s("%d", &emp.salary);
+
+	printf("이름: %s\n", emp.name);
+	printf("고유번호: %d\n", emp.id);
+	printf("급여정보: %d", emp.salary);
+	*/
+
+	// 코드 62
+	/*
+	struct employee emp[3];
+
+	for (int i = 0; i < 3; i++) {
+		printf("사원 %d 이름을 입력하세요: ", i + 1);
+		scanf_s("%s", emp[i].name, 50);
+
+		printf("사원 %d 고유번호를 입력하세요: ", i + 1);
+		scanf_s("%d", &emp[i].id);
+
+		printf("사원 %d 급여정보를 입력하세요: ", i + 1);
+		scanf_s("%d", &emp[i].salary);
+	}
+
+	for (int i = 0; i < 3; i++) {
+		printf("사원 %d\n", i + 1);
+		printf("이름: %s\n", emp[i].name);
+		printf("고유번호: %d\n", emp[i].id);
+		printf("급여정보: %d\n", emp[i].salary);
+		printf("\n");
+	}
+	*/
+
+	
 
 	return 0;
-
+	
 }
 
 void calculator(int* a, int* b, int* sum, int* sub, int* mult, int* div, int* rem)
@@ -507,5 +1036,69 @@ void leapYear(int* a)
 	}
 	else {
 		printf("error");
+	}
+}
+
+void diceGame()
+{
+	int a, b, c;
+	scanf_s("%d %d %d", &a, &b, &c);
+
+	int prize;
+
+	if (a == b && b == c) {
+		prize = 10000 + a * 1000;
+	}
+	else if (a == b) {
+		prize = 1000 + a * 100;
+	}
+	else if (b == c) {
+		prize = 1000 + b * 100;
+	}
+	else if (c == a) {
+		prize = 1000 + c * 100;
+	}
+	else {
+		int max = a;
+		if (b > max) max = b;
+		if (c > max) max = c;
+
+		prize = max * 100;
+	}
+
+	printf("%d\n", prize);
+}
+int bep(int* fcost, int* vcost, int* sell)
+{
+	if (*sell <= *vcost) {
+		return -1;
+	}
+	return (*fcost / (*sell - *vcost))+1;
+}
+
+int sugar(int* weight)
+{	
+	int n = *weight;
+	int cnt = 0;
+
+	while (n % 5 != 0 && n >= 0) {
+		n -= 3;
+		cnt++;
+	}
+	if (n < 0) {
+		return -1;
+	}
+	else {
+		return cnt + n / 5;
+	}
+
+	return 0;
+}
+
+void factorial(int* n, int* res)
+{
+	*res = 1;
+	for (int i = 1; i <= *n; i++) {
+		*res *= i;
 	}
 }
